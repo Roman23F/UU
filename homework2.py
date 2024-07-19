97581df2e214a0c5eab6fe01a9aa90157dbb79ea
Roman23F <roma312b@gmail.com>
2024-07-20 01:32:26 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,10 @@
+tasks = 12
+hours = 1.5
+corse_name = 'Python'
+time_per_task = (hours/tasks)
+print(
+    str(corse_name + ','),
+    'всего задач:', tasks,
+    'затрачено часов:', hours,
+    'среднее время выполнения:', time_per_task
+     )
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Роман'
+print(name)
+age = 25
+print(age)
+new_age = age + 10
+print(new_age)
+is_student = bool(True)
+print(is_student)
