f7798408b9458aa7d185b23550b60e80f85a9158
Roman23F <roma312b@gmail.com>
2024-07-27 22:15:51 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,15 @@
+my_dict = {'Петя': 1998, 'Вася': 2000, 'Саша': 2001}
+print(my_dict)
+print(my_dict['Петя'])
+print(my_dict.get('Леня'))
+my_dict.update({'Вика': 2005, 'Аня': 2003})
+print(my_dict)
+print(my_dict.pop('Вася'))
+print(my_dict)
+my_set = {2, 2, '1', '1'}
+print(my_set)
+my_set.add(3)
+my_set.add(True)
+print(my_set)
+my_set.remove(3)
+print(my_set)
